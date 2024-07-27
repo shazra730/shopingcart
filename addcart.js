5ac026f014483684adcf256561b74e9033969bfa
shazra730 <iit21084@std.uwu.ac.lk>
2024-07-27 12:50:10 +0530

    shoppingcart

---
+++ addcart.js
@@ -0,0 +1,420 @@
+
+
+const product = [
+    {
+        id: 0,
+        image: 'image/1.jpg',
+        title: 'Party wear dress',
+        price: 20,
+    },
+    {
+        id: 1,
+        image: 'image/AA.jpg',
+        title: 'Polo T-Shirt',
+        price: 8,
+    },
+    {
+        id: 2,
+        image: 'image/A.jpg',
+        title: 'Lace off shoulder denim frock ',
+        price: 12,
+    },
+    {
+        id: 3,
+        image: 'image/2.jpg',
+        title: 'Stylish Frock',
+        price: 15,
+    },
+    {
+        id: 4,
+        image: 'image/BB.jpg',
+        title: 'chinese Collar Kurta',
+        price: 15,
+    },
+    {
+        id: 5,
+        image: 'image/B.jpg',
+        title: 'Printed casual Shirt for boys',
+        price: 10,
+    },
+    {
+        id: 6,
+        image: 'image/3.jpg',
+        title: 'Puff sleeve frock',
+        price: 14,
+    },
+    {
+        id: 7,
+        image: 'image/CC.jpg',
+        title: 'Striped Polo T_Shirt',
+        price: 10,
+    },
+    {
+        id: 8,
+        image: 'image/C.jpg',
+        title: 'Baby Suit ',
+        price: 11,
+    },
+    {
+        id: 9,
+        image: 'image/4.jpg',
+        title: 'scarf collar top',
+        price: 9,
+    },
+    {
+        id: 10,
+        image: 'image/DD.jpg',
+        title: 'Slim fit casual Blazer',
+        price: 22,
+    },
+    {
+        id: 11,
+        image: 'image/D.jpg',
+        title: 'Children Shirt and shorts',
+        price: 15,
+    },
+    {
+        id: 12,
+        image: 'image/5.jpg',
+        title: 'Long kurti',
+        price: 13,
+    },
+    {
+        id: 13,
+        image: 'image/EE.jpg',
+        title: 'Marl crew neck T-Shirt',
+        price: 8,
+    },
+    {
+        id: 14,
+        image: 'image/E.jpg',
+        title: 'Summer Style dress for Girls',
+        price: 9,
+    },
+    {
+        id: 15,
+        image: 'image/6.jpeg',
+        title: 'party frock',
+        price: 17,
+    },
+    {
+        id: 16,
+        image: 'image/FF.jpg',
+        title: 'Grey Contrast Polo T-Shirt',
+        price: 9,
+    },
+    {
+        id: 17,
+        image: 'image/F.jpg',
+        title: 'Boys Overcoat Shirt with T-Shirt ',
+        price: 10,
+    },
+    {
+        id: 18,
+        image: 'image/7.jpg',
+        title: 'Party wear indian dress',
+        price: 18,
+    },
+    {
+        id: 19,
+        image: 'image/GG.jpg',
+        title: 'Handloom Sarong',
+        price: 7,
+    },
+    {
+        id: 20,
+        image: 'image/G.jpg',
+        title: 'Floral casual Dress',
+        price: 15,
+    },
+    {
+        id: 21,
+        image: 'image/8.jpg',
+        title: 'Short frock ',
+        price: 9,
+    },
+    {
+        id: 22,
+        image: 'image/HH.jpg',
+        title: 'Stretch chino short',
+        price: 5,
+    },
+    {
+        id: 23,
+        image: 'image/H.jpg',
+        title: 'Linen frock',
+        price: 8,
+    },
+    {
+        id: 24,
+        image: 'image/9.jpg',
+        title: 'Short frock with court',
+        price: 10,
+    },
+    {
+        id: 25,
+        image: 'image/II.jpg',
+        title: 'Handloom Sarong',
+        price: 7,
+    },
+    {
+        id: 26,
+        image: 'image/I.jpg',
+        title: 'Boys Long sleeve Shirt',
+        price: 10,
+    },
+    {
+        id: 27,
+        image: 'image/10.jpg',
+        title: 'High waist skirt',
+        price: 8,
+    },
+    {
+        id: 28,
+        image: 'image/JJ.jpg',
+        title: 'lawful casual Shirt',
+        price: 11,
+    },
+    {
+        id: 29,
+        image: 'image/J.jpg',
+        title: 'Mandarin collar high low Blouse',
+        price: 10,
+    },
+    {
+        id: 30,
+        image: 'image/11.jpg',
+        title: 'Givo striped T-shirt dress',
+        price: 10,
+    },
+    {
+        id: 31,
+        image: 'image/KK.jpg',
+        title: 'Office Pant',
+        price: 10,
+    },
+    {
+        id: 32,
+        image: 'image/K.jpg',
+        title: 'pink Butterfly dress',
+        price: 8,
+    },
+    {
+        id: 33,
+        image: 'image/12.jpg',
+        title: 'Smart Jeggings',
+        price: 6,
+    },
+    {
+        id: 34,
+        image: 'image/LL.jpg',
+        title: 'Sleep wear for mens',
+        price: 8,
+    },
+    {
+        id: 35,
+        image: 'image/L.jpg',
+        title: 'Baby sul suit',
+        price: 15,
+    },
+    {
+        id: 36,
+        image: 'image/13.jpg',
+        title: 'Midi length tights Jeggings',
+        price: 7,
+    },
+    {
+        id: 37,
+        image: 'image/MM.jpg',
+        title: 'printed Shirt',
+        price: 12,
+    },
+    {
+        id: 38,
+        image: 'image/M.jpg',
+        title: 'Casual Clothing Set',
+        price: 14,
+    },
+    {
+        id: 39,
+        image: 'image/14.jpg',
+        title: 'High rise denim',
+        price: 7,
+    },
+    {
+        id: 40,
+        image: 'image/NN.jpg',
+        title: 'Handloom Sarong',
+        price: 7,
+    },
+    {
+        id: 41,
+        image: 'image/N.jpg',
+        title: 'off shoulder dress',
+        price: 9,
+    },
+    {
+        id: 42,
+        image: 'image/15.jpg',
+        title: 'Formal pants',
+        price: 10,
+    },
+    {
+        id: 43,
+        image: 'image/OO.jpg',
+        title: 'pajama Short',
+        price: 5,
+    },
+    {
+        id: 44,
+        image: 'image/O.jpg',
+        title: 'Casual frock',
+        price: 8,
+    },
+    {
+        id: 45,
+        image: 'image/16.jpg',
+        title: 'Pleated Skirt',
+        price: 10,
+    },
+    {
+        id: 46,
+        image: 'image/PP.jpg',
+        title: 'Cotton AOP T-shirt',
+        price: 10,
+    },
+    {
+        id: 47,
+        image: 'image/P.jpg',
+        title: 'T-shirt and Bip pant set',
+        price: 12,
+    },
+    {
+        id: 48,
+        image: 'image/17.jpg',
+        title: 'Print Maxi Skirt',
+        price: 13,
+    },
+    {
+        id: 49,
+        image: 'image/QQ.jpg',
+        title: 'Handloom Sarong',
+        price: 7,
+    },
+    {
+        id: 50,
+        image: 'image/Q.jpg',
+        title: 'Front bow Tie Cropped Blouse with short',
+        price: 12,
+    },
+    {
+        id: 51,
+        image: 'image/18.jpg',
+        title: 'high waist skirt',
+        price: 9,
+    },
+    {
+        id: 52,
+        image: 'image/RR.jpg',
+        title: 'Solid Casual Grey Shirt',
+        price: 11,
+     } ,
+        {
+            id: 53,
+            image: 'image/R.jpg',
+            title: 'long sleeve t-shirt denim suit',
+            price: 14,
+        },
+        {
+            id: 54,
+            image: 'image/19.jpg',
+            title: 'Ladies Top',
+            price: 9,
+        },
+        {
+            id: 55,
+            image: 'image/SS.jpg',
+            title: 'Plain Polo T-shirt',
+            price: 8,
+        },
+        {
+            id: 56,
+            image: 'image/TT.jpg',
+            title: 'Slim Fit Trouser',
+            price: 9,
+        },
+        {
+            id: 57,
+            image: 'image/UU.jpg',
+            title: 'Checked Shirt',
+            price: 9,
+        
+    },
+    {
+        id: 58,
+        image: 'image/VV.jpg',
+        title: 'Slim Fit Trouser',
+        price: 9,
+    }
+    
+];
+
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>$ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+
+            );
+        }).join('');
+    }
+
+    
+}
